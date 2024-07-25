d5176bbe567f54e163b65983f0ced4089fa1598a
Chris Baldelomar <chris@webplant.media>
2024-07-25 16:32:39 -0500

    update

--- _data/fonts/heading.tsx
+++ _data/fonts/heading.tsx
@@ -3,52 +3,52 @@ import localFont from 'next/font/local';
 export const _headingFont = localFont({
   src: [
     {
-      path: '../../styles/fonts/Rubik/Rubik-ExtraBold.woff2',
+      path: '../../styles/fonts/OpenSans/OpenSans-ExtraBold.woff2',
       weight: '900',
       style: 'normal',
     },
     {
-      path: '../../styles/fonts/Rubik/Rubik-ExtraBoldItalic.woff2',
+      path: '../../styles/fonts/OpenSans/OpenSans-ExtraBoldItalic.woff2',
       weight: '900',
       style: 'italic',
     },
     {
-      path: '../../styles/fonts/Rubik/Rubik-Bold.woff2',
+      path: '../../styles/fonts/OpenSans/OpenSans-Bold.woff2',
       weight: '700',
       style: 'normal',
     },
     {
-      path: '../../styles/fonts/Rubik/Rubik-BoldItalic.woff2',
+      path: '../../styles/fonts/OpenSans/OpenSans-BoldItalic.woff2',
       weight: '700',
       style: 'italic',
     },
     {
-      path: '../../styles/fonts/Rubik/Rubik-SemiBold.woff2',
+      path: '../../styles/fonts/OpenSans/OpenSans-SemiBold.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../../styles/fonts/Rubik/Rubik-SemiBoldItalic.woff2',
+      path: '../../styles/fonts/OpenSans/OpenSans-SemiBoldItalic.woff2',
       weight: '500',
       style: 'italic',
     },
     {
-      path: '../../styles/fonts/Rubik/Rubik-Regular.woff2',
+      path: '../../styles/fonts/OpenSans/OpenSans-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../../styles/fonts/Rubik/Rubik-Italic.woff2',
+      path: '../../styles/fonts/OpenSans/OpenSans-Italic.woff2',
       weight: '400',
       style: 'italic',
     },
     {
-      path: '../../styles/fonts/Rubik/Rubik-Light.woff2',
+      path: '../../styles/fonts/OpenSans/OpenSans-Light.woff2',
       weight: '300',
       style: 'normal',
     },
     {
-      path: '../../styles/fonts/Rubik/Rubik-LightItalic.woff2',
+      path: '../../styles/fonts/OpenSans/OpenSans-LightItalic.woff2',
       weight: '300',
       style: 'italic',
     },
