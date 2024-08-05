dd2250da65e9860b2bccf24d29f5739d84532c73
Chris Baldelomar <chris@webplant.media>
2024-08-05 01:36:52 -0500

    update

--- components/blocks/core-group-hero-1.tsx
+++ components/blocks/core-group-hero-1.tsx
@@ -1,3 +1,5 @@
+import Iconify from '@components/iconify';
+import ArrowLongRightIcon from '@iconify/icons-heroicons/arrow-long-right';
 import classNames from 'classnames';
 import { getVarsFromHTML } from '@utils/tools';
 import SwiperInit from '@components/scripts/swiper-init';
@@ -8,7 +10,7 @@ export const CoreGroupHero1 = ({ node, className, props }) => {
   let swiperId = 'swiper-' + useId(); // Generate a unique ID
   swiperId = swiperId.replace(/:/g, '-'); // Sanitize the ID
 
-  console.log(data);
+  console.log();
 
   return (
     <div className={classNames(className, 'relative overflow-clip')}>
@@ -29,45 +31,73 @@ export const CoreGroupHero1 = ({ node, className, props }) => {
       <div className="relative flex flex-row !max-w-screen-3xl py-40">
         <div className="w-7/12">
           <div id={swiperId} className="relative swiper">
-            <div className="swiper-wrapper">
+            <div className="swiper-wrapper items-center flex py-14">
               {data.wpBlockCover?.wpBlockGroup &&
                 data.wpBlockCover.wpBlockGroup.map(
                   (group: any, index: number) => {
-                    console.log(group);
                     return (
                       <div
                         key={`hero-1-group-${index}`}
-                        className="swiper-slide flex flex-col"
+                        className="swiper-slide"
                       >
-                        {group?.h4?.text && (
-                          <strong className="mb-7 leading-tight text-3xl font-bold text-white">
-                            {group.h4.text}
-                          </strong>
-                        )}
-                        {group?.h1?.text && (
-                          <h1 className="mb-7 leading-tight text-4xl md:text-5xl lg:text-6xl text-white font-bold">
-                            {group.h1.text}
-                          </h1>
-                        )}
-                        {group?.h2?.text && (
-                          <h2 className="mb-7 leading-tight text-4xl md:text-5xl lg:text-6xl text-white font-bold">
-                            {group.h2.text}
-                          </h2>
-                        )}
-                        {group?.p?.text && (
-                          <p className="mb-7 text-lg font-bold text-white">
-                            {group?.p?.text}
-                          </p>
-                        )}
+                        <div className="flex flex-col gap-10">
+                          {group?.h4?.text && (
+                            <strong className="mb-0 !leading-tight text-2xl text-white tracking-[0.5em] uppercase font-normal flex flex-row gap-[0.5em] items-center">
+                              {group.h4.text}
+                              <Iconify
+                                icon={ArrowLongRightIcon}
+                                className="flex-shrink-0 h-auto w-10"
+                              />
+                            </strong>
+                          )}
+                          {group?.h1?.text && (
+                            <h1 className="mb-0 !leading-tight text-4xl md:text-5xl lg:text-6xl text-white font-bold">
+                              {group.h1.text}
+                            </h1>
+                          )}
+                          {group?.h2?.text && (
+                            <h2 className="mb-0 !leading-tight text-4xl md:text-5xl lg:text-6xl text-white font-bold">
+                              {group.h2.text}
+                            </h2>
+                          )}
+                          {group?.p?.text && (
+                            <p className="mb-0 text-xl font-bold !leading-normal text-white">
+                              {group?.p?.text}
+                            </p>
+                          )}
+                        </div>
                       </div>
                     );
                   }
                 )}
             </div>
-            <div className="absolute bottom-0 right-0 swiper-pagination flex justify-end cursor-pointer [&>.swiper-pagination-bullet:last-child]:after:content-none [&>.swiper-pagination-bullet]:after:px-3 [&>.swiper-pagination-bullet]:after:content-['/'] [&>.swiper-pagination-bullet]:text-white text-lg [&>.swiper-pagination-bullet-active]:font-bold [&>.swiper-pagination-bullet-active]:!text-primary-main [&>.swiper-pagination-bullet]:after:font-normal [&>.swiper-pagination-bullet]:after:text-white"></div>
+            <div className="z-10 absolute bottom-0 right-0 swiper-pagination flex justify-end [&>.swiper-pagination-bullet]:cursor-pointer [&>.swiper-pagination-bullet:last-child]:after:content-none [&>.swiper-pagination-bullet]:after:px-3 [&>.swiper-pagination-bullet]:after:content-['/'] [&>.swiper-pagination-bullet]:text-white text-lg [&>.swiper-pagination-bullet-active]:font-bold [&>.swiper-pagination-bullet-active]:!text-primary-main [&>.swiper-pagination-bullet]:after:font-normal [&>.swiper-pagination-bullet]:after:text-white"></div>
           </div>
         </div>
-        <div className="w-5/12"></div>
+        <div className="w-5/12">
+          {data.wpBlockCover?.wpBlockButtons?.wpBlockButton?.a?.text && (
+            <div className="circle-text relative w-40 h-40 mx-auto">
+              {data.wpBlockCover.wpBlockButtons.wpBlockButton.a.text
+                .split('')
+                .map((letter: string, index: number) => {
+                  const length =
+                    data.wpBlockCover.wpBlockButtons.wpBlockButton.a.text
+                      .length;
+
+                  const spacing = Math.round(360 / length);
+                  const angle = spacing * index;
+
+                  return (
+                    <span
+                      className={`absolute inset-0 flex items-center justify-center rotate-[${angle}deg] origin-center`}
+                    >
+                      {letter}
+                    </span>
+                  );
+                })}
+            </div>
+          )}
+        </div>
       </div>
       <SwiperInit swiperId={swiperId} />
     </div>
