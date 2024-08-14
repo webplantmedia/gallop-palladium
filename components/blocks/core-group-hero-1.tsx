5263fb3d96a53bcfd74767155f93442a0873a6d7
Chris Baldelomar <chris@webplant.media>
2024-08-14 16:28:45 -0500

    update

--- components/blocks/core-group-hero-1.tsx
+++ components/blocks/core-group-hero-1.tsx
@@ -22,7 +22,6 @@ export const CoreGroupHero1 = ({ node, className, props }) => {
   let brand = { ...data.wpBlockGroup[0]?.wpBlockCover[0] };
   let info = { ...data.wpBlockGroup[0]?.wpBlockCover[1] };
   console.log(brand);
-  console.log(info);
 
   return (
     <Fragment>
@@ -106,23 +105,40 @@ export const CoreGroupHero1 = ({ node, className, props }) => {
       </div>
       <div className="!max-w-none bg-secondary-main">
         <div className="flex flex-col xl:flex-row !max-w-screen-3xl px-4 sm:px-8 mx-auto gap-0">
-          <div className="py-14 px-4 bg-primary-main text-primary-contrast w-4/12 -mt-20 relative z-30 rounded-t-md overflow-hidden">
+          <div className="py-14 px-8 bg-primary-main text-primary-contrast w-4/12 -mt-20 relative z-20 rounded-t-md overflow-hidden">
             <div className="-z-10 bg-primary-main/90 absolute inset-0"></div>
-            <h2 className="mb-2 leading-tight text-2xl md:text-3xl w-full text-center text-primary-contrast">
+            <h2 className="mb-7 leading-tight text-2xl md:text-3xl w-full text-center text-primary-contrast">
               {brand.h2.text}
             </h2>
+            <table className="min-w-full divide-y divide-white/10">
+              <thead></thead>
+              <tbody className="divide-y divide-white/10">
+                {brand.table?.tbody?.tr &&
+                  brand.table.tbody.tr.map((item: any, index: number) => {
+                    return (
+                      <tr key={`brand-item-${index}`}>
+                        <td className="whitespace-nowrap px-3 py-4 text-sm text-white">
+                          {item.td[0].text}
+                        </td>
+                        <td className="whitespace-nowrap px-3 py-4 text-sm text-white text-right">
+                          {item.td[1].text}
+                        </td>
+                      </tr>
+                    );
+                  })}
+              </tbody>
+            </table>
           </div>
           <div className="w-8/12 flex flex-col xl:flex-row">
             {info &&
               info.wpBlockGroup?.length &&
               info.wpBlockGroup.map((item: any, index: number) => {
-                console.log(item);
                 return (
                   <a
                     href={item.p?.a?.href}
-                    key={`info-item-${index + 1}`}
+                    key={`info-item-${index}`}
                     className={classNames(
-                      'py-20 px-12 text-white w-1/3 flex flex-col gap-10 shrink-0 hover:!bg-white/10'
+                      'py-20 px-12 text-white w-1/3 flex flex-col gap-10 shrink-0 hover:!bg-white/10 justify-center'
                     )}
                     style={{
                       backgroundColor: `rgba(255,255,255,0.0${index + 1})`,
