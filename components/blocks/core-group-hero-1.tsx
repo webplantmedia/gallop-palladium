4d830f760ad7ab53672bd22c99ea42fcedc2c132
Chris Baldelomar <chris@webplant.media>
2024-08-14 08:48:30 -0500

    update

--- components/blocks/core-group-hero-1.tsx
+++ components/blocks/core-group-hero-1.tsx
@@ -6,44 +6,14 @@ import SwiperInit from '@components/scripts/swiper-init';
 import { useId } from 'react';
 import { CoreGroupHero1Client } from './core-group-hero-1-client';
 
-const appendVimeoParams = (url: string) => {
-  const urlObj = new URL(url);
-  urlObj.searchParams.set('muted', '1');
-  urlObj.searchParams.set('title', '0');
-  urlObj.searchParams.set('byline', '0');
-  urlObj.searchParams.set('portrait', '0');
-  urlObj.searchParams.set('background', '1');
-  return urlObj.toString();
-};
-
 export const CoreGroupHero1 = ({ node, className, props }) => {
   const data = getVarsFromHTML(node);
   let swiperId = 'swiper-' + useId(); // Generate a unique ID
   swiperId = swiperId.replace(/:/g, '-'); // Sanitize the ID
-  let videoSrc: string = '';
-  let vimeoVideo = false;
-  if (data.wpBlockEmbed?.iframe?.src) {
-    videoSrc = appendVimeoParams(data.wpBlockEmbed.iframe.src);
-    vimeoVideo = true;
-  } else if (data.wpBlockCover?.video?.src) {
-    videoSrc = data.wpBlockCover.video.src;
-  }
 
   return (
-    <div className={classNames(className, 'relative overflow-hidden')}>
-      {vimeoVideo && (
-        <div className="absolute inset-0 w-full h-full overflow-hidden !p-0 !max-w-none bg-black">
-          <iframe
-            className={classNames(
-              'max-h-[calc(100cqh)] h-[500%] w-[500%]',
-              'absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 pointer-events-none'
-            )}
-            src={videoSrc}
-            allow="autoplay; fullscreen"
-          ></iframe>
-        </div>
-      )}
-      {!vimeoVideo && data.wpBlockCover?.video && (
+    <div className={classNames(className, 'relative overflow-clip')}>
+      {data.wpBlockCover?.video && (
         <video
           className={classNames(
             'w-full object-cover object-center h-full absolute inset-0 !max-w-none !p-0'
@@ -52,15 +22,15 @@ export const CoreGroupHero1 = ({ node, className, props }) => {
           muted
           loop
           playsInline
-          src={videoSrc}
+          src={data.wpBlockCover.video.src}
           data-object-fit="cover"
         ></video>
       )}
       <div className="absolute inset-0 h-full w-full !max-w-none bg-black/70"></div>
-      <div className="relative flex flex-row !max-w-screen-3xl py-32">
-        <div className="w-7/12">
-          <div id={swiperId} className="swiper">
-            <div className="swiper-wrapper items-start flex py-14">
+      <div className="relative flex flex-col xl:flex-row !max-w-screen-3xl py-32">
+        <div className="w-full xl:w-7/12 flex justify-center">
+          <div id={swiperId} className="swiper max-w-[950px] xl:max-w-none">
+            <div className="swiper-wrapper items-start flex mb-20">
               {data.wpBlockCover?.wpBlockGroup &&
                 data.wpBlockCover.wpBlockGroup.map(
                   (group: any, index: number) => {
@@ -80,7 +50,7 @@ export const CoreGroupHero1 = ({ node, className, props }) => {
                             </strong>
                           )}
                           {group?.h1?.text && (
-                            <h1 className="mb-0 !leading-tight text-5xl md:text-6xl lg:text-7xl text-white font-bold">
+                            <h1 className="mb-0 !leading-tight text-5xl md:text-6xl lg:text-6xl 3xl:text-7xl text-white font-bold">
                               {group.h1.text}
                               {group.h1?.em?.text && (
                                 <span className="text-stroke text-stroke-white block">
@@ -90,7 +60,7 @@ export const CoreGroupHero1 = ({ node, className, props }) => {
                             </h1>
                           )}
                           {group?.h2?.text && (
-                            <h2 className="mb-0 !leading-tight text-5xl md:text-6xl lg:text-7xl text-white font-bold">
+                            <h2 className="mb-0 !leading-tight text-5xl md:text-6xl lg:text-6xl 3xl:text-7xl text-white font-bold">
                               {group.h2.text}
                               {group.h2?.em?.text && (
                                 <span className="text-stroke text-stroke-white block">
@@ -113,7 +83,7 @@ export const CoreGroupHero1 = ({ node, className, props }) => {
             <div className="z-10 absolute bottom-0 left-0 swiper-pagination flex justify-end [&>.swiper-pagination-bullet]:cursor-pointer [&>.swiper-pagination-bullet:last-child]:after:content-none [&>.swiper-pagination-bullet]:after:px-3 [&>.swiper-pagination-bullet]:after:content-['/'] [&>.swiper-pagination-bullet]:text-white [&>.swiper-pagination-bullet-active]:!text-primary-main text-lg [&>.swiper-pagination-bullet-active>span]:!rounded-sm [&>.swiper-pagination-bullet-active]:!flex [&>.swiper-pagination-bullet-active>span]:!px-2 [&>.swiper-pagination-bullet-active>span]:!block [&>.swiper-pagination-bullet-active>span]:!bg-primary-contrast [&>.swiper-pagination-bullet]:after:font-normal [&>.swiper-pagination-bullet]:after:text-white"></div>
           </div>
         </div>
-        <div className="w-5/12 flex items-start justify-center pt-32">
+        <div className="w-full xl:w-5/12 flex items-start justify-center pt-32">
           {data.wpBlockCover?.wpBlockButtons?.wpBlockButton?.a?.text && (
             <CoreGroupHero1Client data={data} />
           )}
