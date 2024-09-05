4b1d3d70468601986f2af7d88b505785aa89098f
Chris Baldelomar <chris@webplant.media>
2024-09-05 02:54:16 -0500

    udpate

--- components/blocks/core-group-card-1.tsx
+++ components/blocks/core-group-card-1.tsx
@@ -9,16 +9,17 @@ import { CoreParagraph, CoreHeading } from '@components/blocks';
 
 export const CoreGroupCard1 = ({ node, className, props }) => {
   const data = getVarsFromHTML(node);
-  // console.log('DATA', data);
+  console.log('DATA', data);
 
   let img: any = {};
   let href = '#';
-  let button = data.wpBlockButtons?.wpBlockButton?.a;
-  if (data.a?.img) {
-    img = { ...data.a.img };
-    href = data.a.href;
+  let button = data?.div?.div?.a;
+  let figure = data?.figure;
+  if (figure?.a?.img) {
+    img = { ...figure.a.img };
+    href = figure.a.href;
   } else {
-    img = { ...data.img };
+    img = { ...figure.img };
   }
 
   const src = getVimeoIframeSrc(href);
@@ -54,15 +55,15 @@ export const CoreGroupCard1 = ({ node, className, props }) => {
       </VideoPopup>
       <div className="bg-base-card px-7 pb-7 h-full flex flex-col">
         <CoreHeading tag="h4" props={data} className="is-style-h3 !mb-2 !mt-4">
-          {data?.h4?.text}
+          {data?.h4?.jsx}
         </CoreHeading>
-        <CoreParagraph className="">{data.p?.text}</CoreParagraph>
+        <CoreParagraph className="">{data.p?.jsx}</CoreParagraph>
         <div className="text-center rounded-md shadow-sm flex items-center justify-center mt-auto bg-primary-main text-primary-contrast hover:bg-primary-light">
           <a
             className="text-base py-3 px-5 w-full flex justify-center items-center gap-2"
             href={button?.href}
           >
-            {button?.text}
+            {button?.jsx}
             <Iconify
               icon={ArrowInsertIcon}
               className="flex-shrink-0 h-auto w-6 rotate-90 inline"
