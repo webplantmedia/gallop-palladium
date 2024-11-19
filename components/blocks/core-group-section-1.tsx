560f53a5b1ed516da2398c0fd9a6ca5eebf12ce0
Chris Baldelomar <chris@webplant.media>
2024-11-18 19:52:38 -0600

    update

--- components/blocks/core-group-section-1.tsx
+++ components/blocks/core-group-section-1.tsx
@@ -3,16 +3,30 @@ import ArrowInsertIcon from '@iconify/icons-material-symbols/arrow-insert';
 import PlayIcon from '@iconify/icons-heroicons/play';
 // import BuildingOfficeIcon from '@iconify/icons-heroicons/building-office';
 import classNames from 'classnames';
-import { getVimeoIframeSrc, replaceWordPressUrlRelative } from '@utils/tools';
+import {
+  getVimeoIframeSrc,
+  replaceWordPressUrlRelative,
+  tailwindGetAlignClasses,
+} from '@utils/tools';
 import { VideoPopup } from '@widgets/video-popup';
 import { CoreParagraph, CoreHeading } from '@components/blocks';
 import { BlockProps } from '@lib/types';
 import { AnimatedNumber } from '@components/widgets/animated-number';
 
 export const CoreGroupSection1 = ({ data, className, props }: BlockProps) => {
+  className = tailwindGetAlignClasses(className);
+  const img1 = data?.wpBlockGallery?.wpBlockImage?.img || null;
+  const img2 = data?.wpBlockGallery?.wpBlockImage_2?.img || null;
+  const img3 = data?.wpBlockGallery?.wpBlockImage_3?.img || null;
+  const img4 = data?.wpBlockGallery?.wpBlockImage_4?.img || null;
+
   return (
-    <>
-      <h2 className="text-pretty text-4xl font-medium tracking-tighter text-gray-950 data-[dark]:text-white sm:text-6xl">
+    <div className={className}>
+      <h2
+        className={classNames(
+          'text-pretty text-4xl font-medium tracking-tighter text-gray-950 data-[dark]:text-white sm:text-6xl'
+        )}
+      >
         Helping companies generate revenue.
       </h2>
       <p className="mt-6 max-w-3xl text-2xl font-medium text-gray-500">
@@ -23,7 +37,7 @@ export const CoreGroupSection1 = ({ data, className, props }: BlockProps) => {
         <div className="max-w-lg">
           <h2 className="text-2xl font-medium tracking-tight">Our mission</h2>
           <p className="mt-6 text-sm/6 text-gray-600">
-            At Radiant, we are dedicated to transforming the way revenue
+            At RRadiant, we are dedicated to transforming the way revenue
             organizations source and close deals. Our mission is to provide our
             customers with an unfair advantage over both their competitors and
             potential customers through insight and analysis. We’ll stop at
@@ -40,34 +54,82 @@ export const CoreGroupSection1 = ({ data, className, props }: BlockProps) => {
         </div>
         <div className="pt-20 lg:row-span-2 lg:-mr-16 xl:mr-auto">
           <div className="-mx-8 grid grid-cols-2 gap-4 sm:-mx-16 sm:grid-cols-4 lg:mx-0 lg:grid-cols-2 lg:gap-4 xl:gap-8">
-            <div className="aspect-square overflow-hidden rounded-xl shadow-xl outline-1 -outline-offset-1 outline-black/10">
-              <img
-                alt=""
-                src="/company/1.jpg"
-                className="block size-full object-cover"
-              />
-            </div>
-            <div className="-mt-8 aspect-square overflow-hidden rounded-xl shadow-xl outline-1 -outline-offset-1 outline-black/10 lg:-mt-32">
-              <img
-                alt=""
-                src="/company/2.jpg"
-                className="block size-full object-cover"
-              />
-            </div>
-            <div className="aspect-square overflow-hidden rounded-xl shadow-xl outline-1 -outline-offset-1 outline-black/10">
-              <img
-                alt=""
-                src="/company/3.jpg"
-                className="block size-full object-cover"
-              />
-            </div>
-            <div className="-mt-8 aspect-square overflow-hidden rounded-xl shadow-xl outline-1 -outline-offset-1 outline-black/10 lg:-mt-32">
-              <img
-                alt=""
-                src="/company/4.jpg"
-                className="block size-full object-cover"
-              />
-            </div>
+            {img1 && (
+              <div className="aspect-square overflow-hidden rounded-xl shadow-xl outline-1 -outline-offset-1 outline-black/10">
+                <img
+                  className={classNames(
+                    img1.className,
+                    'block size-full object-cover'
+                  )}
+                  loading="lazy"
+                  src={img1.src}
+                  style={img1.style}
+                  width={parseInt(img1.width)}
+                  height={parseInt(img1.height)}
+                  srcSet={img1.srcSet}
+                  sizes={img1.sizes}
+                  alt={img1.alt}
+                  title={img1.title}
+                />
+              </div>
+            )}
+            {img2 && (
+              <div className="-mt-8 aspect-square overflow-hidden rounded-xl shadow-xl outline-1 -outline-offset-1 outline-black/10 lg:-mt-32">
+                <img
+                  className={classNames(
+                    img2.className,
+                    'block size-full object-cover'
+                  )}
+                  loading="lazy"
+                  src={img2.src}
+                  style={img2.style}
+                  width={parseInt(img2.width)}
+                  height={parseInt(img2.height)}
+                  srcSet={img2.srcSet}
+                  sizes={img2.sizes}
+                  alt={img2.alt}
+                  title={img2.title}
+                />
+              </div>
+            )}
+            {img3 && (
+              <div className="aspect-square overflow-hidden rounded-xl shadow-xl outline-1 -outline-offset-1 outline-black/10">
+                <img
+                  className={classNames(
+                    img3.className,
+                    'block size-full object-cover'
+                  )}
+                  loading="lazy"
+                  src={img3.src}
+                  style={img3.style}
+                  width={parseInt(img3.width)}
+                  height={parseInt(img3.height)}
+                  srcSet={img3.srcSet}
+                  sizes={img3.sizes}
+                  alt={img3.alt}
+                  title={img3.title}
+                />
+              </div>
+            )}
+            {img4 && (
+              <div className="-mt-8 aspect-square overflow-hidden rounded-xl shadow-xl outline-1 -outline-offset-1 outline-black/10 lg:-mt-32">
+                <img
+                  className={classNames(
+                    img4.className,
+                    'block size-full object-cover'
+                  )}
+                  loading="lazy"
+                  src={img4.src}
+                  style={img4.style}
+                  width={parseInt(img4.width)}
+                  height={parseInt(img4.height)}
+                  srcSet={img4.srcSet}
+                  sizes={img4.sizes}
+                  alt={img4.alt}
+                  title={img4.title}
+                />
+              </div>
+            )}
           </div>
         </div>
         <div className="max-lg:mt-16 lg:col-span-1">
@@ -103,6 +165,6 @@ export const CoreGroupSection1 = ({ data, className, props }: BlockProps) => {
           </dl>
         </div>
       </section>
-    </>
+    </div>
   );
 };
