c78197a1beccddfea46bbc9bdce4d1c6b8dcd021
Niel Wostan <wostanniel@gmail.com>
2025-01-15 09:49:40 -0600

    added icon text

--- components/blocks/gallop-icon-text.tsx
+++ components/blocks/gallop-icon-text.tsx
@@ -2,6 +2,7 @@ import { replaceWordPressUrlRelative } from '@utils/tools';
 import DevicePhoneMobileIcon from '@iconify/icons-heroicons/device-phone-mobile';
 import EnvelopeIcon from '@iconify/icons-heroicons/envelope';
 import ChatBubbleBottomCenterTextIcon from '@iconify/icons-heroicons/chat-bubble-bottom-center-text';
+import ChatBubbleLeft from '@iconify/icons-heroicons/chat-bubble-left';
 import Iconify from '@components/iconify';
 import Link from 'next/link';
 import classNames from 'classnames';
@@ -37,6 +38,14 @@ export const GallopIconText = ({
         menuIcon = (
           <Iconify
             icon={ChatBubbleBottomCenterTextIcon}
+            className="flex-shrink-0 h-6 w-6 mr-2"
+          />
+        );
+        break;
+      case 'icon-text':
+        menuIcon = (
+          <Iconify
+            icon={ChatBubbleLeft}
             className="flex-shrink-0 h-6 w-6 mr-2"
           />
         );
