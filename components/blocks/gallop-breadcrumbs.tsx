0f064ac5861edf1dc397a3383ec3a82967c5ffc1
Chris Baldelomar <chris@webplant.media>
2025-01-15 16:52:21 -0600

    small updat

--- components/blocks/gallop-breadcrumbs.tsx
+++ components/blocks/gallop-breadcrumbs.tsx
@@ -7,7 +7,7 @@ import parse from 'html-react-parser';
 
 export function GallopBreadcrumbs({ breadcrumbs, className }: any) {
   const breadcrumbsLength = breadcrumbs.length;
-  let classNameList = 'relative w-full wp-block-group pt-16';
+  let classNameList = 'relative w-full wp-block-group mt-14';
   if (className.includes('is-style-bottom-spacing')) {
     classNameList += ' mb-14';
   }
