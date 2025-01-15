ac041289d244fbdb2755090b27f816e596f681fe
Niel Wostan <wostanniel@gmail.com>
2025-01-15 13:33:21 -0600

    update

--- components/blocks/core-list.tsx
+++ components/blocks/core-list.tsx
@@ -19,10 +19,11 @@ import { Alignment } from '@components/common';
 export const coreList = (
   domNode: Element,
   options: HTMLReactParserOptions,
-  className: string
+  className: string,
+  type: keyof JSX.IntrinsicElements
 ) => {
   const op: HTMLReactParserOptions = {
-    replace(domNode) {
+    replace(domNode, index) {
       if (domNode instanceof Element && domNode.attribs) {
         const props: HTMLAttributeProps = castToHTMLAttributeProps(
           domNode.attribs
@@ -32,12 +33,18 @@ export const coreList = (
         if (domNode.name === 'li') {
           return (
             <li className="flex gap-x-3 items-start">
-              <span className="w-3 shrink-0 mt-[0.45rem]">
-                <Iconify
-                  className="text-primary-main dmh:text-modern-primary-main w-3 h-3"
-                  icon={dotMarkIcon}
-                />
-              </span>
+              {type == 'ul' ? (
+                <span className="w-3 shrink-0 mt-[0.45rem]">
+                  <Iconify
+                    className="text-primary-main dmh:text-modern-primary-main w-3 h-3"
+                    icon={dotMarkIcon}
+                  />
+                </span>
+              ) : (
+                <span className="text-primary-main font-bold">
+                  {index + 1}.
+                </span>
+              )}
               <span className="">
                 {domToReact(domNode.children as DOMNode[], options)}
               </span>
@@ -52,21 +59,23 @@ export const coreList = (
 
   const content = domToReact(domNode.children as DOMNode[], op);
 
-  return <CoreList content={content} className={className} />;
+  return <CoreList content={content} className={className} type={type} />;
 };
 
 export const CoreList = ({
   content,
   className,
+  type,
 }: {
   content: any;
   className: string;
+  type: keyof JSX.IntrinsicElements;
 }) => {
   className = tailwindAlignClasses(className);
 
   return (
     <Alignment
-      as="ul"
+      as={type}
       className={classNames(
         className,
         'leading-normal mb-7 flex flex-col gap-2'
