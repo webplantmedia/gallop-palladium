c549ed27296a4e45a120f53a8eb447d7b950c37f
Niel Wostan <wostanniel@gmail.com>
2025-01-20 14:03:49 -0600

    update

--- components/navbar/mobile-menu.tsx
+++ components/navbar/mobile-menu.tsx
@@ -85,6 +85,7 @@ export default function MobileMenu({ menu }: { menu: any }) {
                     prefetch={true}
                     href={'/our-team/'}
                     scroll={true}
+                    onClick={closeModal}
                     className={classNames(
                       'text-base-contrast border border-base-contrast/20 align-center inline-flex w-full justify-start rounded-md py-3 px-4 bg-base-body cursor-pointer hover:bg-white/30 items-center gap-x-2 dmh:bg-modern-base-card dmh:hover:bg-white/30'
                     )}
